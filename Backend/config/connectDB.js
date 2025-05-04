b784a888a6b61f6c9bd0b974626729b3c4595324
Shatakshi Sharma <156279870+Shatakshi-Sharma12@users.noreply.github.com>
2025-05-04 21:50:26 +0530

    Delete Backend directory

--- Backend/config/connectDB.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB=async ()=>{
-    await mongoose.connect(process.env.MONGODB)
-    .then(
-        console.log("DB Connected Successfully")
-    )
-}
